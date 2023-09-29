adcb21ac278ca5d0e37da3fd6147d945c453da76
bilal <bilalhussain42201@gmail.com>
2023-09-29 22:26:03 +0500

    create age python

---
+++ age.py
@@ -0,0 +1,10 @@
+print("\tWhat is you age")
+age = int(input("Enter Your Age: "))
+
+if age <= 18:
+        print("You can not vote")
+elif age == 18:
+        print("We will think about you")
+else:
+        print("You can vote")
+               
